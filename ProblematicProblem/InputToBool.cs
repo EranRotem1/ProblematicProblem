3fe892d6138c4b6a7d5e2c71aa77365b70a8d0cc
Eran Rotem <eranrotem123@gmail.com>
2023-02-24 09:28:15 -0600

    init commit

---
+++ ProblematicProblem/InputToBool.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblematicProblem
+{
+    public class InputToBool
+    {
+        public static bool InToBool()
+        {
+            var user = Console.ReadLine();
+            switch (user.ToLower())
+            {
+                case "yes":
+                    return true;
+
+                case "no":
+                    return false;
+                default:
+                    Console.WriteLine("invalid input, please try again: ");
+                    return InToBool();
+            }
+        }
+    }
+}
